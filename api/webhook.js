9123f7a7726b9998f1c623f3fe1af40eee1065ca
Alex <alexx.j.jackson@gmail.com>
2025-09-26 16:14:13 -0700

    Fix API exports - use ES6 exports for Vercel

--- api/webhook.js
+++ api/webhook.js
@@ -128,7 +128,7 @@ async function addToGoogleSheets(data, session) {
   }
 }
 
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
